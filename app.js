0a4d817c119c426c388264839bf3a060f36deb3e
kirza <110294781+kkirza@users.noreply.github.com>
2022-08-20 20:28:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const slides = document.querySelectorAll('.slide')
-
+const body = document.body
 for (const slide of slides){
 	slide.addEventListener('click', () =>{
 		clearActiveClasses()
+		body.style.backgroundImage  = slide.style.backgroundImage 
 		slide.classList.add('active')
 	})
 }
